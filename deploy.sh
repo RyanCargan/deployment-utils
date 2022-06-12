532d686a5502ff7502314bec4637924694086663
Ryan Cargan <ryancargan@gmail.com>
2022-06-12 19:28:19 +0530

    Fix duplicate command

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ cd projects/react-app/ &&
 pnpm i &&
 pnpm build &&
 rsync -a --delete dist/ /var/www/static/ &&
-pm2 start go_api &&
 echo "Done..."
 #rsync -a --delete dist/ $STATIC_DEST_DIR
 # Note: Missing env vars referenced with a trailing slash seem to count as the root folder (shouldn't be any risk unless you run as root for some reason but FYI just in case)
