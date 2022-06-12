56ed564acc7fdc6a7dbc49bfc7cd78b63dae653c
Ryan Cargan <ryancargan@gmail.com>
2022-06-12 13:43:09 +0530

    Add pm2 commands

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
+cd ../workspace/monorepo/ &&
+pm2 stop go_api &&
+git pull &&
+pm2 start go_api &&
+cd projects/react-app/ &&
+pnpm build &&
+rsync -a --delete dist/ /var/www/static/ &&
+pm2 start go_api &&
 echo "Done..."
 #rsync -a --delete dist/ $STATIC_DEST_DIR
 # Note: Missing env vars referenced with a trailing slash seem to count as the root folder (shouldn't be any risk unless you run as root for some reason but FYI just in case)
